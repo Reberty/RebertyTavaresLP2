cf1d8daf150883cfa5bfaf8cc462d9133c5174a7
Reberty <rebertytavares@hotmail.com>
2018-04-30 16:55:29 -0300

    Aula revisão

---
+++ Aulas/Aula 1/Aula 1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int  max, div;
+
+            
+
+            Console.WriteLine("informe o número máximo");
+            max = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("informe o divisor");
+            div = int.Parse(Console.ReadLine());
+
+            for (int i = div; i <= max; i+=div)
+            {
+               
+
+               
+                if(i % div == 0)
+                {
+                    Console.WriteLine(" é multiplo de {0} !", i );
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
